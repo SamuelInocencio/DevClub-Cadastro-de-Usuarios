f52910916a7f36556bb2f0539ee010feadb523a3
SamuelInocencio <samuelferreirainocencio@gmail.com>
2024-12-13 10:18:49 -0300

    Instalado React Dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import Home from './pages/Home'
 import { GlobalStyles } from './styles/GlobalStyles.js'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalStyles />
     <Home />
+
   </StrictMode>,
 )

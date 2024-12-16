3e9f1e9f9841295a665ca0d0791d682738a8de8f
SamuelInocencio <samuelferreirainocencio@gmail.com>
2024-12-16 19:06:30 -0300

    Projeto React concluído com sucesso!!!

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
 
 import { Button } from "../../components/Button/styles"
@@ -10,6 +11,7 @@ import Trash from '../../assets/trash.svg'
 
 function ListUsers() {
     const [users, setUsers] = useState([])
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function getUsers() {
@@ -19,6 +21,14 @@ function ListUsers() {
         }
         getUsers()
     }, [])
+
+    async function deleteUsers(id) {
+        await api.delete(`/usuarios/${id}`)
+
+        const updatedUsers = users.filter(user => user.id !== id)
+
+        setUsers(updatedUsers)
+    }
 
 
     return (
@@ -35,12 +45,12 @@ function ListUsers() {
                             <p>{user.age}</p>
                             <p>{user.email}</p>
                         </div>
-                        <TrashIcon src={Trash} />
+                        <TrashIcon src={Trash} alt='icone-lixeira' onClick={() => deleteUsers(user.id)} />
                     </CardUsers>
                 ))}
 
             </ContainerUsers >
-            <Button type="button" alt='icone-lixeira'>Voltar</Button>
+            <Button type="button" onClick={() => navigate('/')} >Voltar</Button>
         </Container>
     )
 }

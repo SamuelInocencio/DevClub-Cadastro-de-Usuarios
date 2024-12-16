cba783d4cfbf998ac8e172501cc900e8308cd4bf
SamuelInocencio <samuelferreirainocencio@gmail.com>
2024-12-16 17:43:40 -0300

    conlcuida aula 26 - Estilizando a página

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -26,6 +26,7 @@ text-align: center;
 font-size: 38px;
 font-style: normal;
 font-weight: 600;
+margin-top: 30px;
 `
 
 export const ContainerInputs = styled.div `
--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -3,7 +3,8 @@ import api from "../../services/api";
 
 import { Button } from "../../components/Button/styles"
 import TopBackground from "../../components/TopBackground"
-import { Container, Title, ContainerUsers, CardUsers, TrashIcon } from "./styles";
+import { Container, Title, ContainerUsers, CardUsers, TrashIcon, AvatarUser } from "./styles";
+import Trash from '../../assets/trash.svg'
 
 
 
@@ -28,17 +29,18 @@ function ListUsers() {
             <ContainerUsers>
                 {users.map((user) => (
                     <CardUsers key={user.id}>
+                        <AvatarUser src={`https://avatar.iran.liara.run/public?username=${user.id}`} />
                         <div>
-                            <p>{user.name}</p>
-                            <p>{user.email}</p>
+                            <h3>{user.name}</h3>
                             <p>{user.age}</p>
+                            <p>{user.email}</p>
                         </div>
-                        <TrashIcon />
+                        <TrashIcon src={Trash} />
                     </CardUsers>
                 ))}
 
             </ContainerUsers >
-            <Button type="button">Voltar</Button>
+            <Button type="button" alt='icone-lixeira'>Voltar</Button>
         </Container>
     )
 }
